054a4a35918f0490b90d70af519a38aa9a07eaf2
Helloa <160602656+nhelloa@users.noreply.github.com>
2024-11-14 10:46:58 -0300

    Add files via upload

--- dadosGerais.js
+++ dadosGerais.js
@@ -6,14 +6,28 @@ async function vizualizando(){
 
     console.log(dados)  
     const cursos = Object.keys(dados)
-    const cursoMaisVotado = cursos[0]
+    const votos = Object.values(dados)
+    const esporteMaisVotado = cursos[0]
     const quantidadeDeVoto = Object.values(dados) [0]
+
+    const data = [
+        {
+            x:esporteMaisVotado,
+            y:votos,
+            type: 'bar'
+        }
+    ]
+
+
+
+
+
 
     let paragrafo = document.createElement('p')
 
     paragrafo.classList.add('caixa-grafico_texto')
 
-    paragrafo.innerHTML = 'Nesta pesquisa, buscou-se compreender qual o esporte mais praticado pela população brasileira. 0 ${esporteMaisVotado} foi o esporte mais votado entre todos, com um total de ${quantidaDedeVotos} votos, em uma pesquisa que teve um total de um milhão de participantes.'
+    paragrafo.innerHTML = `Nesta pesquisa, buscou-se compreender qual o esporte mais praticado pela população brasileira. 0 ${esporteMaisVotado} foi o esporte mais votado entre todos, com um total de ${quantidaDedeVotos} votos, em uma pesquisa que teve um total de um milhão de participantes.`
 
     let caixa = document.getElementById('caixa-grafico')
     caixa.appendChild(paragrafo)
@@ -22,5 +36,10 @@ async function vizualizando(){
     console.log(cursoMaisVotado)
     console.log(quantidadeDeVoto)
 
+    const grafico = document.createElement('div')
+    grafico.className = 'grafico'
+    document.getElementById('caixa-grafico').appendChild(grafico)
+    plotly.newPlotly(grafico, data)
 }
+
 vizualizando()
